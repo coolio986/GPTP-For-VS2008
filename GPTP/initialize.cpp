bd46cbb79d25d9492b8ce580c7b5cb3954cde423
KYSXD <dracojan_12@hotmail.com>
2015-09-21 16:24:41 -0500

    Commented buttonset hooks due to bug on sub menus

--- GPTP/initialize.cpp
+++ GPTP/initialize.cpp
@@ -41,7 +41,7 @@
 
 #include "hooks/infestation.h"
 #include "hooks/siege_transform.h"
-#include "hooks/interface/buttonsets.h"
+//#include "hooks/interface/buttonsets.h"
 #include "hooks/interface/selection.h"
 
 #include "AI/spellcasting.h"
@@ -67,7 +67,7 @@ BOOL WINAPI Plugin::InitializePlugin(IMPQDraftServer *lpMPQDraftServer) {
   
   hooks::injectInfestationHooks();
   hooks::injectSiegeTransformHooks();
-  hooks::injectButtonSetHooks();
+//  hooks::injectButtonSetHooks();
   hooks::injectSelectMod();
 
   hooks::injectApplyUpgradeFlags();
